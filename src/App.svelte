d3c18618859340e948898f370f99f88c610ccd4b
Kay Putra <kaspr.works@gmail.com>
2022-03-26 22:32:32 +1100

    going through the fundamentals of working in a component

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,46 @@
 <script>
-	export let name;
+	let firstName = 'Kay'
+	let lastName = 'Putra'
+	let color = 'blue'
+	let showText = false
+	let users = [
+		{
+			id: 1,
+			name: 'John'
+		},
+		{
+			id: 2,
+			name: 'Barb'
+		},
+		{
+			id: 3,
+			name: 'Nick'
+		}
+	]
+
+	$: name = firstName + ' ' + lastName
+
+	const toggle = () => {
+		color = color === 'blue' ? color = 'red' : color = 'blue'
+		showText = !showText
+		users = [...users, {id:4, name: 'Jen'}]
+	}
+	
 </script>
 
 <main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<h1 style="color: {color}">Hello {name}!</h1>
+	{#if showText}
+		<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	{:else}
+		<p>No text</p>
+	{/if}
+
+	<button on:click={toggle}>Click</button>
+
+	{#each users as user (user.id)}
+		<h3>{user.id}: {user.name}</h3>
+	{/each}
 </main>
 
 <style>
