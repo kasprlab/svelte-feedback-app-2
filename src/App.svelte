5a30384a1f6a191421986d2eaec168ea3655966a
Kay Putra <kaspr.works@gmail.com>
2022-03-28 21:32:04 +1100

    creating components

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,26 @@
 <script>
-	
+		import FeedbackList from './components/FeedbackList.svelte'
+
+		let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed lectus vestibulum mattis ullamcorper velit sed.'
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed lectus vestibulum mattis ullamcorper velit sed.'
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sed lectus vestibulum mattis ullamcorper velit sed.'
+		}
+	]
 
 </script>
 
-<main>
-	<h1>Hello</h1>
+<main class="container">
+	<FeedbackList {feedback}/>
 </main>
