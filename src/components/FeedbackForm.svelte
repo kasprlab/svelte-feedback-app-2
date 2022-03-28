6da624103c45e0e86e95ec12442f0fccd93e49e9
Kay Putra <kaspr.works@gmail.com>
2022-03-28 23:17:15 +1100

    adding feedback form and button components (incomplete)

---
+++ src/components/FeedbackForm.svelte
@@ -0,0 +1,49 @@
+<script>
+import Card from './Card.svelte'
+</script>
+
+<Card>
+  <header>
+    <h2>How would you rate your service with us?</h2>
+  </header>
+  <form action="">
+    <!-- Rating Select-->
+    <div class="input-group">
+      <input type="text" Placeholder="Tell us something that keeps you coming back">
+      <button>Send</button>
+    </div>
+  </form>
+</Card>
+
+<style>
+  header {
+    max-width: 400px;
+    margin: auto;
+  }
+
+  header h2 {
+    text-align: center;
+    font-size: 22px;
+    font-weight: 600;
+  }
+
+  .input-group {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid #ccc;
+    padding: 8px 10px;
+    border-radius: 8px;
+    margin-top: 15px;
+  }
+
+  input {
+    flex-grow: 2;
+    border: none;
+    font-size: 16px;
+  }
+
+  input:focus {
+    outline: none;
+  }
+
+</style>
