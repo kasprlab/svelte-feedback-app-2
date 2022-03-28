03d760e67b24d6b93d714165fc6a5c93fccc03b3
Kay Putra <kaspr.works@gmail.com>
2022-03-28 21:41:49 +1100

    create handle delete event in feedback item

--- src/components/FeedbackItem.svelte
+++ src/components/FeedbackItem.svelte
@@ -2,13 +2,17 @@
   import Card from './Card.svelte'
 
   export let item
+
+  const handleDelete = (itemID) => {
+    console.log(itemID)
+  }
 </script>
 
 <Card>
   <div class="num-display">
     {item.rating}
   </div>
-  <div class="close">X</div>
+  <button class="close" on:click={() => handleDelete(item.id)}>X</button>
   <p class="text display">
     {item.text}
   </p>
