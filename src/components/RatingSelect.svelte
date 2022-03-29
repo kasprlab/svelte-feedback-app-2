30523aebcce84cc8fac84b169cd63d3e26a92e30
Kay Putra <kaspr.works@gmail.com>
2022-03-29 19:46:29 +1100

    add rating select component css

--- src/components/RatingSelect.svelte
+++ src/components/RatingSelect.svelte
@@ -10,7 +10,7 @@
   }
 </script>
 
-<ul>
+<ul class="rating">
   <li>
     <input type="radio" id="num1" name="rating" value="1" on:change={onChange} checked={selected===1}>
     <label for="num1">1</label>
@@ -52,3 +52,51 @@
     <label for="num10">10</label>
   </li>
 </ul>
+
+<style>
+  .rating {
+    display: flex;
+    justify-content: space-around;
+    align-items: center;
+    margin: 30px 0;
+  }
+
+  .rating li {
+    position: relative;
+    background: #f4f4f4;
+    width: 50px;
+    height: 50px;
+    padding: 10px;
+    text-align: center;
+    font-size: 19px;
+    border-radius: 50%;
+    border: 1 #eee solid;
+  }
+
+  .rating li label {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 50px;
+    height: 50px;
+    padding: 10px;
+    border-radius: 50%;
+    transform: translate(-50%, -50%);
+    cursor: pointer;
+  }
+
+  .rating li:hover {
+    background: #ff6a95;
+    color: #fff;
+  }
+
+  [type='radio'] {
+    opacity: 0;
+  }
+
+  [type='radio']:checked ~ label {
+    background: #ff6a95;
+    color: #fff;
+  }
+
+</style>
